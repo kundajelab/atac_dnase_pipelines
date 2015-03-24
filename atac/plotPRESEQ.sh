c5a6c74a42d4643ba16c09e5c3504cc579fb9dbc
Grey Christoforo <greysAcademicCode@users.noreply.github.com>
2015-03-24 08:53:06 +0100

    add scg3 PRESEQ compatibility
    also switch from rendering pdf plots to png ones for compatibility

--- atac/plotPRESEQ.sh
+++ atac/plotPRESEQ.sh
@@ -4,6 +4,8 @@ if hash module 2>/dev/null; then
    if [ -n "$CLUSTER_IS_PBS" ]; then
       module add gnuplot/5.0.0
       module add preseq/1.0.2
+   else
+      module add preseq
    fi
    module add samtools/1.2
 fi
@@ -20,14 +22,17 @@ samtools sort -Ttmp -l0 -Obam "$infile" -o "${sortedInfile}"
 
 # run preseq
 preseq lc_extrap -B -o "${sortedInfile}.preseq.dat" "${sortedInfile}" -v 2> "${sortedInfile}.preseq.log"
+rm sortedInfile
 
 # plot the results
 # maximum number of reads on x axis in plot (in millions)
 XMAX=500
 
+titleName=$(basename $infile) 
+
 cat > preseq.gnu <<EOF
-set terminal pdf 
-set output '$infile.preseq.pdf'
+set terminal png 
+set output '$infile.preseq.png'
 set nokey
 set style line 1 linewidth 5
 set mxtics 2
@@ -35,7 +40,7 @@ set grid ytics xtics mxtics
 set xrange [0:${XMAX}]
 set xlabel "Number of Reads [millions]"
 set ylabel "Expected Distinct Reads [millions]"
-set title "PRESEQ Results for $infile"
+set title "PRESEQ Results for $titleName"
 plot '$infile.sort.bed.preseq.dat' using (\$1/1000000):(\$2/1000000) with lines linestyle 1
 EOF
 
