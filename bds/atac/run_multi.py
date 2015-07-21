0234c44b304f44c74f37e9dcd1c9c80b06bec484
Chuan-Sheng Foo <csfoo@cs.stanford.edu>
2015-07-21 02:58:00 -0700

    Allow printing of commands instead of executing
    * Add logging for messages

--- bds/atac/run_multi.py
+++ bds/atac/run_multi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import errno
+import logging
 import os
 import sys
 
@@ -29,13 +30,16 @@ def parse_args():
     parser.add_argument('-t', '--threads',
                         help='Number of threads (default: 1)',
                         type=int, default=1)
+    parser.add_argument('-p', '--printscript',
+                        help='Prints commands to file (does not run them)',
+                        default='')
     return parser.parse_args()
 
 
 ATAC_BDS = '/users/leepc12/code/pipelines/bds/atac/atac.bds'
-MOD_DEF = ('"bowtie/2.2.4; samtools/1.2; bedtools/2.21.0; picard-tools/1.129; '
+MOD_DEF = ('bowtie/2.2.4; samtools/1.2; bedtools/2.21.0; picard-tools/1.129; '
            'ucsc_tools/3.0.9; MACS2/2.1.0; java/latest; preseq/1.0.2; '
-           'texlive/2013"')
+           'texlive/2013')
 GENOME2GENOME_SIZE = {
     'hg19': 'hs',
     'mm9': 'mm',
@@ -46,9 +50,20 @@ GENOME2CHROM_SIZE = {
 }
 
 
+# TODO: allow default params for index, tssfile given genome
 if __name__ == "__main__":
-    args = parse_args()
+    logging.basicConfig(level=logging.INFO)
 
+    args = parse_args()
+    print args
+
+    if args.printscript:
+        print_fp = open(args.printscript, 'w')
+
+    mkdir_p(args.output_directory)
+    cd(args.output_directory)
+
+    workers = []
     with open(args.sample_file) as fp:
         for line in fp:
             line = line.strip().split('\t')
@@ -56,10 +71,27 @@ if __name__ == "__main__":
 
             sample_output_dir = '%s-%s' % (sample_id, description)
 
-            cd(args.output_directory)
+            logging.info('Processing %s (genome: %s)' %
+                         (sample_output_dir, genome))
+
             mkdir_p(sample_output_dir)
             genome_size = GENOME2GENOME_SIZE[genome]
             chrom_size = GENOME2CHROM_SIZE[genome]
-            bds('-s', args.system, ATAC_BDS, index, read1, read2, args.threads,
-                genome_size, chrom_size, tssfile, sample_output_dir,
-                '-mod', MOD_DEF)
+
+            if args.printscript:
+                print_fp.write('%s\n' % ' '.join(
+                    map(str, ['bds', '-s', args.system, ATAC_BDS,
+                              index, read1, read2, args.threads,
+                              genome_size, chrom_size, tssfile,
+                              sample_output_dir, '-mod "', MOD_DEF, '"'])))
+            else:
+                workers.append(
+                    bds('-s', args.system, ATAC_BDS, index, read1, read2,
+                        args.threads, genome_size, chrom_size, tssfile,
+                        sample_output_dir, '-mod', MOD_DEF, _bg=True))
+
+    if not args.printscript:
+        for worker in workers:
+            worker.wait()
+    else:
+        print_fp.close()
