1576eea8eaa30b53c8116c6e1796e8ccfbb01282
Jin Lee <leepc12@gmail.com>
2016-04-11 20:26:13 -0700

    added csem to bowtie2 and changed shifted tagalign name .shift. -> .tn5., macs2 peak call generates broad peak and gapped peak.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,25 +6,31 @@ conda config --add channels bioconda
 conda config --add channels astro
 
 conda create -n bds_atac --file requirements.txt -y
+conda create -n bds_atac_r2 --file requirements_r2.txt -y
 conda create -n bds_atac_py3 --file requirements_py3.txt -y
 
+
+function add_to_activate {
+  for i in "${CONTENTS[@]}"; do
+    if [ $(grep "$i" "$CONDA_INIT" | wc -l ) == 0 ]; then
+      echo $i >> "$CONDA_INIT"
+    fi
+  done
+}
 
 ############ install additional packages
 source activate bds_atac
 
-CONDA_ACTIVATE=$(which activate)
+if [ $? != 0 ]; then
+  echo Anaconda environment not found!
+  exit
+fi
+
 CONDA_BIN=$(dirname $(which activate))
-CONDA_EXTRA="$CONDA_BIN/extras"
-
-mkdir -p $CONDA_EXTRA
-
-function add_to_activate {
-  for i in "${CONTENTS[@]}"; do
-    if [ $(grep "$i" "$CONDA_ACTIVATE" | wc -l ) == 0 ]; then
-      echo $i >> "$CONDA_ACTIVATE"
-    fi
-  done
-}
+CONDA_EXTRA="$CONDA_BIN/../extra"
+CONDA_ACTIVATE_D="$CONDA_BIN/../etc/conda/activate.d"
+CONDA_INIT="$CONDA_ACTIVATE_D/init.sh"
+mkdir -p $CONDA_EXTRA $CONDA_ACTIVATE_D
 
 ### BDS
 CONTENTS=("export PATH=\$PATH:\$HOME/.bds")
@@ -33,6 +39,32 @@ add_to_activate
 ### PICARDROOT
 CONTENTS=("export PICARDROOT=$CONDA_BIN")
 add_to_activate
+
+##### preseq ==2.0.2 
+cd $CONDA_EXTRA
+rm -rf preseq
+git clone https://github.com/smithlabcode/preseq --recursive
+cd preseq
+git checkout tags/v2.0.2
+make all
+cd $CONDA_BIN
+ln -s $CONDA_EXTRA/preseq/preseq preseq
+
+source deactivate
+
+
+source activate bds_atac_r2
+
+if [ $? != 0 ]; then
+  echo Anaconda environment not found!
+  exit
+fi
+
+CONDA_BIN=$(dirname $(which activate))
+CONDA_EXTRA="$CONDA_BIN/../extra"
+CONDA_ACTIVATE_D="$CONDA_BIN/../etc/conda/activate.d"
+CONDA_INIT="$CONDA_ACTIVATE_D/init.sh"
+mkdir -p $CONDA_EXTRA $CONDA_ACTIVATE_D
 
 #### install R 2.x.x packages
 cd $CONDA_EXTRA
@@ -57,62 +89,7 @@ wget https://phantompeakqualtools.googlecode.com/files/ccQualityControl.v.1.1.ta
 tar zxvf ccQualityControl.v.1.1.tar.gz
 rm -f ccQualityControl.v.1.1.tar.gz
 chmod 755 -R phantompeakqualtools
-cd $CONDA_BIN
 CONTENTS=("export PATH=\$PATH:$CONDA_EXTRA/phantompeakqualtools")
 add_to_activate
-
-##### preseq ==2.0.2 
-cd $CONDA_EXTRA
-rm -rf preseq
-git clone https://github.com/smithlabcode/preseq --recursive
-cd preseq
-git checkout tags/v2.0.2
-make all
-cd $CONDA_BIN
-ln -s $CONDA_EXTRA/preseq/preseq preseq
-
-### texlive ==2013
-cd $CONDA_EXTRA
-mkdir -p texlive
-wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -N
-tar zxvf install-tl-unx.tar.gz
-rm -f install-tl-unx.tar.gz
-cd install-tl*
-echo "selected_scheme scheme-basic" > texlive_profile.txt
-echo "TEXDIR $CONDA_EXTRA/texlive/2015" >> texlive_profile.txt
-echo "TEXMFCONFIG ~/.texlive2015/texmf-config" >> texlive_profile.txt
-echo "TEXMFHOME ~/texmf" >> texlive_profile.txt
-echo "TEXMFLOCAL $CONDA_EXTRA/texlive/texmf-local" >> texlive_profile.txt
-echo "TEXMFSYSCONFIG $CONDA_EXTRA/texlive/2015/texmf-config" >> texlive_profile.txt
-echo "TEXMFSYSVAR $CONDA_EXTRA/texlive/2015/texmf-var" >> texlive_profile.txt
-echo "TEXMFVAR ~/.texlive2015/texmf-var" >> texlive_profile.txt
-echo "binary_x86_64-linux 1" >> texlive_profile.txt
-echo "collection-basic 1" >> texlive_profile.txt
-echo "collection-latex 1" >> texlive_profile.txt
-echo "in_place 0" >> texlive_profile.txt
-echo "option_adjustrepo 1" >> texlive_profile.txt
-echo "option_autobackup 1" >> texlive_profile.txt
-echo "option_backupdir tlpkg/backups" >> texlive_profile.txt
-echo "option_desktop_integration 1" >> texlive_profile.txt
-echo "option_doc 1" >> texlive_profile.txt
-echo "option_file_assocs 1" >> texlive_profile.txt
-echo "option_fmt 1" >> texlive_profile.txt
-echo "option_letter 0" >> texlive_profile.txt
-echo "option_menu_integration 1" >> texlive_profile.txt
-echo "option_path 0" >> texlive_profile.txt
-echo "option_post_code 1" >> texlive_profile.txt
-echo "option_src 1" >> texlive_profile.txt
-echo "option_sys_bin /usr/local/bin" >> texlive_profile.txt
-echo "option_sys_info /usr/local/info" >> texlive_profile.txt
-echo "option_sys_man /usr/local/man" >> texlive_profile.txt
-echo "option_w32_multi_user 1" >> texlive_profile.txt
-echo "option_write18_restricted 1" >> texlive_profile.txt
-echo "portable 0" >> texlive_profile.txt
-./install-tl -profile texlive_profile.txt
-cd $CONDA_BIN
-PDFLATEX=$(find . -name 'pdflatex' | head -n 1)
-ln -s $CONDA_BIN/$PDFLATEX pdflatex
-
-
 
 source deactivate
